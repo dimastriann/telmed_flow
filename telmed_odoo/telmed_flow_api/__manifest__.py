193bca9d3d79307b978845623e0f6b0dc7705492
dimastriann <dimastriannugraha@gmail.com>
2026-04-25 15:17:48 +0700

    feat: added third party module
    - initialize base_hospital_management module with core hospital models, security policies, and UI assets.

--- telmed_odoo/telmed_flow_api/__manifest__.py
+++ telmed_odoo/telmed_flow_api/__manifest__.py
@@ -1,26 +1,26 @@
 {
-    'name': "TelMedFlow API",
-    'summary': "API for TelMedFlow",
-    'description': """API for TelMedFlow""",
-    'author': "dimastriann",
-    'website': "https://github.com/dimastriann",
+    "name": "TelMedFlow API",
+    "summary": "API for TelMedFlow",
+    "description": """API for TelMedFlow""",
+    "author": "dimastriann",
+    "website": "https://github.com/dimastriann",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    "category": "Uncategorized",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['fastapi'],
+    "depends": ["fastapi"],
     # always loaded
-    'data': [
+    "data": [
         #'security/ir.model.access.csv',
-        'data/fastapi_data.xml',
-        'views/views.xml',
-        'views/templates.xml',
+        "data/fastapi_data.xml",
+        "views/views.xml",
+        "views/templates.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
-    "license": "LGPL-3",
+    "license": "AGPL-3",
 }
